0d7fb74e352d8e05aa95aa02d130ccdf785a7b72
nojustrusovas <nojus.trusovas@gmail.com>
2023-11-19 14:23:01 +0000

    Initial Commit

--- cae/_main.py
+++ cae/_main.py
@@ -1,16 +1,16 @@
 import sys
+import typing
 
-from subwindows import _game_configuration, _chessboard, homepage
+from subwindows import _homepage, _game_configuration, _chessboard
 
 import PySide6.QtCore
 from PySide6.QtWidgets import QApplication, QMainWindow, QStackedWidget
 from PySide6.QtGui import QIcon
 
 
-# Main application window
 class MainWindow(QMainWindow):
-    
-    def __init__(self, application):
+
+    def __init__(self, application: QApplication):
         super().__init__()
 
         self.application = application
@@ -18,57 +18,48 @@ class MainWindow(QMainWindow):
 
         self.subwindow_stack = []
 
-        # Initialise a stacked widget for navigating through sub windows
         self.subwindows = QStackedWidget()
         self.setCentralWidget(self.subwindows)
-        self.initSubWindows()
+        self.instantiateSubwindows()
+        self.setCurrentSubWindow(0)
+        self.centreMainWindow()
 
-        self.centerWindow()
+    def instantiateSubwindows(self):
+        self.subwindow_instances = [
+            _homepage.SubWindow(self), _game_configuration.SubWindow(self), _chessboard.SubWindow(self)
+        ]
 
-        self.moveToWindow(0)
+        for instance in self.subwindow_instances:
+            self.subwindows.addWidget(instance)
 
-    # Instantiate sub window objects and add to stacked widget
-    def initSubWindows(self):
-        self.sub_instances = (
-            homepage.SubWindow(self), _game_configuration.SubWindow(self),
-            _chessboard.SubWindow(self)
-        )
+    def setCurrentSubWindow(self, index):
+        self.subwindow_stack.append(self.subwindows.currentIndex())
+        self.subwindows.setCurrentIndex(index)
+        self.subwindows.currentWidget().initWindow()
 
-        for subwindow in self.sub_instances:
-            self.subwindows.addWidget(subwindow)
+    def goToPreviousSubWindow(self):
+        previous_index = self.subwindow_stack[-1]
+        self.subwindows.setCurrentIndex(previous_index)
+        self.subwindow_stack.pop()
 
-    # Center window proportional to display dimensions
-    def centerWindow(self):
+    def centreMainWindow(self):
         frame = self.frameGeometry()
         center = self.screen().availableGeometry().center()
         frame.moveCenter(center)
         self.move(frame.topLeft())
 
-    # Function for moving to a previous sub window
-    def previousWindow(self):
-        self.subwindows.setCurrentIndex(self.subwindow_stack[-2])
-        self.subwindow_stack.pop(-1)
-        self.subwindows.currentWidget().initWindow()
-
-    # Function for moving to a specific sub window
-    def moveToWindow(self, desired_index):
-        self.subwindows.setCurrentIndex(desired_index)
-        self.subwindow_stack.append(desired_index)
-        self.subwindows.currentWidget().initWindow()
-
     def closeApplication(self):
         self.application.quit()
 
 
-# Handle exceptions
+# Handles top-level exceptions
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
-
-# Program entry point
 if __name__ == '__main__':
     sys.excepthook = except_hook
-    print(f'Running PySide6 v{PySide6.__version__}') # debug log
+    print(f'Running PySide6 v{PySide6.__version__}')
+
     app = QApplication(sys.argv)
     w = MainWindow(app)
     w.show()
